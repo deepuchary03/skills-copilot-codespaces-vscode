6ab4413008459894755855244a24731e8eeb8fc2
deepuchary03 <deepuchary03@gmail.com>
2025-04-02 16:18:02 +0530

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    name: "skills",
+    description: "Skills and abilities of the member",
+    type: "string",
+    required: false,
+    default: "",
+  };
+}
